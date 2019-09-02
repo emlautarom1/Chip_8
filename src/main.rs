d1699b13da6619c5febcaf2812b9e1166cad24a1
lautaroem1 <emlautarom1@gmail.com>
2019-09-01 22:09:47 -0300

    Refactored nested matches with guards

--- src/main.rs
+++ src/main.rs
@@ -111,32 +111,32 @@ use std::error::Error;
 fn main() {
     let mut chip_8_vm = Chip8::new();
 
-    match env::args().nth(1) {
-        Some(path) => {
-            println!("Loading ROM {} ...", path);
-            match fs::read(path) {
-                Ok(content) => {
-                    match chip_8_vm.load_rom_content(content) {
-                        Ok(total_read) => {
-                            println!("ROM loaded successfully. {} bytes were read.", total_read);
-                        }
-                        Err(msg) => {
-                            println!("ERROR: {}", msg);
-                            exit(1);
-                        }
-                    }
-                }
-                Err(err) => {
-                    println!("ERROR: Failed to open the ROM.");
-                    println!("Rust provided the next error message:\n>> {}", err);
-                    exit(1);
-                }
-            }
-        }
+    let path = match env::args().nth(1) {
         None => {
             println!("ERROR: No ROM provided.");
             println!("Usage: ./chip8.exe (path-to-your-rom)");
             exit(1);
         }
+        Some(path) => path
     };
+
+    let rom_binary_content = match fs::read(&path) {
+        Err(msg) => {
+            println!("ERROR: Failed to open the ROM.");
+            println!("Rust provided the next error message:\n>> {}", msg);
+            exit(1);
+        }
+        Ok(content) => content,
+    };
+
+    println!("Loading ROM {} ...", &path);
+    match chip_8_vm.load_rom_content(rom_binary_content) {
+        Err(msg) => {
+            println!("ERROR: {}", msg);
+            exit(1);
+        }
+        Ok(total_read) => {
+            println!("ROM loaded successfully. {} bytes were read.", total_read);
+        }
+    }
 }
